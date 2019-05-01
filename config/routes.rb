0e56fbde5566c6cf19e91f9a64d5983684a2dbcf
major238 <major238@hotmail.co.jp>
2019-05-01 23:19:45 +0900

    ちゃっとスペース　ユーザー管理
    ユーザー管理の機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
 	root 'massage#index'
 	resources :massage
+	resources :users, only: [:edit, :update]
+	resources :groups, only: [:new, :create, :edit, :update] do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
 end
