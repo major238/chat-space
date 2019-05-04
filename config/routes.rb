c38e8283ab289ae653d201082fc74cdf68aa9eef
major238 <major238@hotmail.co.jp>
2019-05-05 02:44:45 +0900

    チャットスペースgroup機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 	root 'massage#index'
-	resources :massage
 	resources :users, only: [:edit, :update]
 	resources :groups, only: [:new, :create, :edit, :update] do
-
+     resources :messages, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 end
